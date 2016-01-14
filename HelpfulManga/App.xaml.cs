ba0a00e43fea2a4ef0a79a5bf080603256ab6212
Kananek Thongkam <info.dvgamer@gmail.com>
2016-01-14 23:33:46 +0700

    Append Project

---
+++ HelpfulManga/App.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.IO;
+using System.Windows;
+
+namespace HelpfulManga
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        private String ApplicationPath = Path.GetDirectoryName(System.Reflection.Assembly.GetEntryAssembly().Location) + "\\";
+
+        private void App_Startup(object sender, StartupEventArgs e)
+        {
+            MainWindow AppMain = new MainWindow();
+            AppMain.Show();
+        }
+    }
+}
---
+++ HelpfulManga/MainWindow.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.IO;
+using System.Windows;
+using iHelpfulEngine;
+using iHelpfulEngine.Collection;
+
+namespace HelpfulManga
+{
+    public partial class MainWindow : Window
+    {
+        dialogAddURL dialogURL = new dialogAddURL();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void AppMain_Loaded(object sender, RoutedEventArgs e)
+        {
+            Point Desktop = new Point(SystemParameters.WorkArea.Width, SystemParameters.WorkArea.Height);
+            this.Left = 10;
+            this.Top = Desktop.Y - (this.Height + 10);
+            this.Topmost = false;
+
+            RequestBuilder nipp = new RequestBuilder("http://www.nipponanime.net/forum/nisekoi/(one-shot)-nisekoi-ch-0-(lightning)/");
+           txtTest.Text = XHR.Connect(nipp).ToString();
+
+            nipp = new RequestBuilder("http://upic.me/i/f1/7bx12.jpg");
+            nipp.Referer = "http://www.nipponanime.net/forum/nisekoi";
+            XHR c = new XHR(nipp);
+            ResponseBuilder a = new ResponseBuilder();
+            do
+            {
+                //File.WriteAllBytes("C:\\Users\\HaKko\\Desktop\\"+Path.GetRandomFileName() +".txt", c.ReceiveByte());
+                Console.WriteLine(XHR.BytesPerSecond(c.ReceivedData));
+            } while (c.ReceivedData > 0);
+        }
+
+        private void AppMain_Closed(object sender, EventArgs e)
+        {
+
+        }
+
+        private void AppMain_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            dialogURL.ShowDialog();
+        }
+    }
+}
